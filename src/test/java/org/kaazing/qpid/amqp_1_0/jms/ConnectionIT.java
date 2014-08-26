2bd5dfdb24d26842797214574b4710aa33be21f3
John Fallows <john.fallows@kaazing.com>
2014-08-25 23:06:44 -0700

    Add test for connection without start before close.

--- src/test/java/org/kaazing/qpid/amqp_1_0/jms/ConnectionIT.java
+++ src/test/java/org/kaazing/qpid/amqp_1_0/jms/ConnectionIT.java
@@ -18,9 +18,18 @@ public class ConnectionIT {
     @Rule
     public RobotRule robot = new RobotRule().setScriptRoot("org/kaazing/robot/scripts/amqp_1_0/jms/connection");
 
+    @Robotic(script = "create.then.close")
+    @Test(timeout = 1000)
+    public void shouldCreateThenClose() throws Exception {
+        ConnectionFactory factory = new ConnectionFactoryImpl("localhost", 5672, null, null, "clientID");
+        Connection connection = factory.createConnection();
+        connection.close();
+        robot.join();
+    }
+
     @Robotic(script = "start.then.close")
     @Test(timeout = 1000)
-    public void shouldConnect() throws Exception {
+    public void shouldStartThenClose() throws Exception {
         ConnectionFactory factory = new ConnectionFactoryImpl("localhost", 5672, null, null, "clientID");
         Connection connection = factory.createConnection();
         connection.start();
@@ -29,9 +38,9 @@ public class ConnectionIT {
     }
 
     @Ignore("check if QPID JMS supports this yet")
-    @Robotic(script = "start.with.credentials")
+    @Robotic(script = "start.with.credentials.then.close")
     @Test(timeout = 1000)
-    public void shouldConnectWithCredentials() throws Exception {
+    public void shouldStartWithCredentialsThenClose() throws Exception {
         ConnectionFactory factory = new ConnectionFactoryImpl("localhost", 5672, null, null, "clientID");
         Connection connection = factory.createConnection("username", "password");
         connection.start();
